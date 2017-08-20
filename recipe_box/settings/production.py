829a776077b67e2b4ac1bf9e0680d6c6bed175f8
Julia Ebert <ebertjulia@gmail.com>
2017-08-20 11:32:07 -0400

    Add production settings

--- recipe_box/settings/production.py
+++ recipe_box/settings/production.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
+# SECRET KEY FOR PRODUCTION
+import os
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+
 from .base import *
 
 DEBUG = False
@@ -9,13 +14,12 @@ try:
 except ImportError:
     pass
 
+# WAGTAIL HEROKU SETTINGS
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-
 DATABASES['default'] = dj_database_url.config()
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
